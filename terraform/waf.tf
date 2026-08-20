e098cd5059111de077d9ae8c756cf98140cce5eb
Amarkarale84 <amarkarale65@gmail.com>
2026-08-21 00:04:09 +0530

    Add AWS WAF protection for CloudFront

---
+++ terraform/waf.tf
@@ -0,0 +1,39 @@
+resource "aws_wafv2_web_acl" "cloudfront" {
+  provider = aws.us_east_1
+  count    = var.enable_cloudfront ? 1 : 0
+
+  name  = "wanderlust-cloudfront-waf"
+  scope = "CLOUDFRONT"
+
+  default_action {
+    allow {}
+  }
+
+  rule {
+    name     = "AWSManagedRulesCommonRuleSet"
+    priority = 10
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "wanderlust-common-rules"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "wanderlust-cloudfront-waf"
+    sampled_requests_enabled   = true
+  }
+}
