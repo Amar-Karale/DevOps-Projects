fa0fa9d184070df531716901b037ab555708ce37
Amarkarale84 <amarkarale65@gmail.com>
2026-08-21 00:04:33 +0530

    Add Terraform variables for CloudWatch and CloudFront

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 }
 
 variable "ami_id" {
-  description = "AMI ID for the EC2 instance"
+  description = "AMI ID for the optional EC2 host"
   type        = string
 }
 
@@ -16,7 +16,7 @@ variable "instance_type" {
 }
 
 variable "key_name" {
-  description = "Existing AWS EC2 key pair name"
+  description = "EC2 key pair name"
   type        = string
 }
 
@@ -27,6 +27,42 @@ variable "public_key" {
 }
 
 variable "ssh_cidr_blocks" {
-  description = "CIDR ranges allowed to SSH to the instance"
+  description = "CIDR ranges allowed to SSH to the instance. Prefer a single trusted public IP /32."
   type        = list(string)
+}
+
+variable "enable_cloudwatch" {
+  description = "Create the CloudWatch log group and EC2 CPU alarm"
+  type        = bool
+  default     = true
+}
+
+variable "cloudwatch_log_retention_days" {
+  description = "CloudWatch log retention period"
+  type        = number
+  default     = 30
+}
+
+variable "enable_cloudfront" {
+  description = "Create the optional CloudFront + WAF edge layer"
+  type        = bool
+  default     = false
+}
+
+variable "cloudfront_origin_domain_name" {
+  description = "DNS name of the frontend load balancer, without protocol or path"
+  type        = string
+  default     = ""
+}
+
+variable "cloudfront_aliases" {
+  description = "Optional custom DNS names for CloudFront"
+  type        = list(string)
+  default     = []
+}
+
+variable "cloudfront_price_class" {
+  description = "CloudFront price class"
+  type        = string
+  default     = "PriceClass_100"
 }
